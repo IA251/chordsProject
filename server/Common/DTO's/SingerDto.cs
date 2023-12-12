dc660068fba43e9c53c785931c963a7cee984ac4
IA251 <153579994+IA251@users.noreply.github.com>
2023-12-13 00:06:19 +0200

    uploaded common

---
+++ server/Common/DTO's/SingerDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.DTO_s
+{
+    public class SingerDto
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+        public string? ImageURL { get; set; }
+
+        public List<SongDto> Songs { get; set; } = new List<SongDto>();
+
+    }
+}
---
+++ server/Common/DTO's/SongDto.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.DTO_s
+{
+    public class SongDto
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Lyrics { get; set; }
+
+        public string? ImageURL { get; set; }
+
+        public List<SingerDto> Singers { get; set; } = new List<SingerDto>();
+    }
+}
